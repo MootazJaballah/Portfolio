fc596b893213d6a3d9cfdc825d14fe3646c2a242
Mootaz Jaballah <mootezjaballah00@gmail.com>
2023-09-15 11:24:18 +0200

    swipe update

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,30 +1,21 @@
-import Loader from 'react-loaders';
-import './index.scss';
-import AnimatedLetters from '../AnimatedLetters'
-import { useState, useEffect } from 'react';
-import { collection, getDocs } from "firebase/firestore";
+import React, { useState, useEffect, useMemo } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../../data/firebase';
-
+import Loader from 'react-loaders';
+import AnimatedLetters from '../AnimatedLetters';
+import './index.scss';
 
 const Portfolio = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
-
-    const [project, setproject] = useState([]);
+    const [project, setProject] = useState([]);
     const [file, setFile] = useState([]);
     const [index, setIndex] = useState(0);
 
-
-
     const fetchPost = async () => {
-
-        await getDocs(collection(db, "projects"))
-            .then((querySnapshot) => {
-                const newData = querySnapshot.docs
-                    .map((doc) => ({ ...doc.data(), id: doc.id }));
-                setproject(newData);
-            })
-
-    }
+        const querySnapshot = await getDocs(collection(db, 'projects'));
+        const newData = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+        setProject(newData);
+    };
 
     useEffect(() => {
         fetchPost();
@@ -34,69 +25,79 @@ const Portfolio = () => {
 
         return () => {
             clearTimeout(timer);
+        };
+    }, []);
+
+    const truncateParagraph = (paragraph) => {
+        if (paragraph.length > 70) {
+            return paragraph.substring(0, 70) + '...';
+        } else {
+            return paragraph;
         }
-    });
+    };
+
+    const images = useMemo(() => {
+        if (file.length > 0) {
+            return file;
+        }
+        if (project.length > 0) {
+            return project[index].images;
+        }
+        return [];
+    }, [file, project, index]);
+
+    const swipeImage = () => {
+        if (images.length > 0) {
+            setIndex((prevIndex) => (prevIndex < images.length - 1 ? prevIndex + 1 : 0));
+        }
+    };
+
+    const openImagePopup = (images) => {
+        setFile(images);
+        setIndex(0);
+    };
+
+    const closeImagePopup = () => {
+        setFile([]);
+        setIndex(0);
+    };
 
     const renderPortfolio = () => {
         return (
             <div className="images-container">
-                {
-                    project.map((port, idx) => {
-                        return (
-                            <div className="image-box" key={idx} onClick={() => {
-                                setFile(port.images);
-                            }}>
-                                <img
-                                    src={port.images[0]}
-                                    className="portfolio-image"
-                                    alt="portfolio" />
-                                <div className="content">
-                                    <p className="title">{port.name}</p>
-                                    <h4 className="description">{port.description}</h4>
-                                    <button
-                                        className="btn"
-                                        onClick={() => window.open(port.githubUrl)}
-                                    >View</button>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
+                {project.map((port, idx) => (
+                    <div className="image-box" key={idx} onClick={() => openImagePopup(port.images)}>
+                        <img src={port.images[0]} className="portfolio-image" alt="portfolio" />
+                        <div className="content">
+                            <p className="title">{port.name}</p>
+                            <h4 className="description">{truncateParagraph(port.description)}</h4>
+                            <button className="btn" onClick={() => window.open(port.githubUrl)}>
+                                View
+                            </button>
+                        </div>
+                    </div>
+                ))}
             </div>
         );
-    }
-
-    const swipeImage = (listImage) => {
-        if (index < (listImage.length - 1)) {
-            setIndex(index + 1);
-        } else {
-            setIndex(0);
-        }
-    }
+    };
 
     return (
         <>
-            <div className='container portfolio-page' >
-                {file.length > 0 ? <div className='popup-media' >
-                    <span onClick={() => {
-                        setFile([]);
-                        setIndex(0);
-                    }}>&times;</span>
-                    <img src={file[index]} alt='popup' onClick={() => swipeImage(file)} />
-                </div> : <div></div>}
-                <h1 className='page-title' >
-                    <AnimatedLetters
-                        letterClass={letterClass}
-                        strArray={'portfolio'.split('')}
-                        idx={15}
-                    />
+            <div className="container portfolio-page">
+                {file.length > 0 && (
+                    <div className="popup-media">
+                        <span onClick={closeImagePopup}>&times;</span>
+                        <img src={images[index]} alt="popup" onClick={swipeImage} />
+                    </div>
+                )}
+                <h1 className="page-title">
+                    <AnimatedLetters letterClass={letterClass} strArray={'portfolio'.split('')} idx={15} />
                 </h1>
                 <div>{renderPortfolio()}</div>
             </div>
-            <Loader type='pacman' />
+            <Loader type="pacman" />
         </>
-
     );
-}
+};
 
 export default Portfolio;
